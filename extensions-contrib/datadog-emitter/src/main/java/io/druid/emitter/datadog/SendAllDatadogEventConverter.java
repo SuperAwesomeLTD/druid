b8cb9cc502e2a00944e6496aa01095ff5a46f2a4
Florian Zysset <florian@zysset.me>
2016-11-30 17:30:07 +0000

    Remove interval and duration to reduce metric usage on datadog

--- extensions-contrib/datadog-emitter/src/main/java/io/druid/emitter/datadog/SendAllDatadogEventConverter.java
+++ extensions-contrib/datadog-emitter/src/main/java/io/druid/emitter/datadog/SendAllDatadogEventConverter.java
@@ -70,7 +70,8 @@ public class SendAllDatadogEventConverter implements DruidToDatadogEventConverte
     Map<String, Object> userDims = serviceMetricEvent.getUserDims();
     List<String> tags = new ArrayList<>();
     for (Map.Entry<String, Object> values : userDims.entrySet()) {
-      if(!values.getKey().contains("context") && !values.getKey().contains("id"))
+      if(!values.getKey().contains("context") && !values.getKey().contains("id")
+              && !values.getKey().contains("duration") && !values.getKey().contains("interval"))
         tags.add(String.format("%s:%s", values.getKey(), values.getValue()));
     }
     for (Map.Entry<String, String> values : this.datadogEmitterConfig.getTagsMap().entrySet()) {
